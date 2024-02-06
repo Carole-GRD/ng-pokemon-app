ae9c000f2e3420cd10b47f13bedebe6ed7b447d4
Carole Gérard <gerard.carole78@gmail.com>
2024-02-06 15:55:11 +0100

    pokemon.ts : type d'un pokemon (class) + mock-pokemon-list.ts : liste de 12 pokémons

--- README.md
+++ README.md
@@ -1,27 +1,30 @@
-# NgPokemonApp
+# Application Pokémon - Angular
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.1.1.
+Cette application Pokémon a été développée dans le cadre de mon apprentissage du framework Angular, en suivant le cours Udemy dédié. Elle représente une étape importante dans mon parcours d'apprentissage, me permettant de mettre en pratique les concepts enseignés tout en explorant les fonctionnalités puissantes offertes par Angular.
 
-## Development server
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
-## Code scaffolding
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+## Version d'Angular CLI
+Ce projet a été généré avec [Angular CLI](https://github.com/angular/angular-cli) version 17.1.1.
 
-## Build
+```js
+ng new ng-pokemon-app --minimal --style=css
+```
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+Il existe d'autres options comme par exemple :
 
-## Running unit tests
+```js
+ng new ng-pokemon-app --no-strict --standalone false --routing false
+```
 
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
 
-## Running end-to-end tests
+Dans le fichier tsconfig.json, ajouter dans "compilerOptions" : 
 
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+```js
+"compilerOptions" : {
+    // ...
+    "strictPropertyInitialization": false,
+    // ...
+}
+```
---
+++ src/app/mock-pokemon-list.ts
@@ -0,0 +1,112 @@
+import { Pokemon } from "./pokemon";
+
+export const POKEMONS: Pokemon[] = [
+    {
+        id: 1,
+        name: "Bulbizarre",
+        hp: 25,
+        cp: 5,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        types: ["Plante", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 2,
+        name: "Salamèche",
+        hp: 28,
+        cp: 6,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+        types: ["Feu"],
+        created: new Date()
+    },
+    {
+        id: 3,
+        name: "Carapuce",
+        hp: 21,
+        cp: 4,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+        types: ["Eau"],
+        created: new Date()
+    },
+    {
+        id: 4,
+        name: "Squirtle",
+        hp: 21,
+        cp: 4,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
+        types: ["Eau"],
+        created: new Date()
+    }, 
+    {
+        id: 5,
+        name: "Charmander",
+        hp: 28,
+        cp: 6,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+        types: ["Feu"],
+        created: new Date()
+    },
+    {
+        id: 6,
+        name: "Ivysaur",
+        hp: 25,
+        cp: 5,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png",
+        types: ["Plante", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 7,
+        name: "Charmeleon",
+        hp: 28,
+        cp: 6,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png",
+        types: ["Feu"],
+        created: new Date()
+    },
+    {
+        id: 8,
+        name: "Wartortle",
+        hp: 21,
+        cp: 4,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png",
+        types: ["Eau"],
+        created: new Date()
+    },
+    {
+        id: 9,
+        name: "Venusaur",
+        hp: 25,
+        cp: 5,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+        types: ["Plante", "Poison"],
+        created: new Date()
+    },
+    {
+        id: 10,
+        name: "Charizard",
+        hp: 28,
+        cp: 6,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png",
+        types: ["Feu"],
+        created: new Date()
+    },
+    {
+        id: 11,
+        name: "Blastoise",
+        hp: 21,
+        cp: 4,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png",
+        types: ["Eau"],
+        created: new Date()
+    },
+    {
+        id: 12,
+        name: "Venusaur",
+        hp: 25,
+        cp: 5,
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png",
+        types: ["Plante", "Poison"],
+        created: new Date()
+    }
+];
