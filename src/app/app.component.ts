f43dae3286b28bb79f7fd7af2b53464e43ca3468
Carole Gérard <gerard.carole78@gmail.com>
2024-02-07 16:56:11 +0100

    Les Templates d'Angular : Créer un flux de données bidirectionnel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,28 @@ import { Pokemon } from './pokemon';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  template: `
-    <h1>Liste de Pokémons</h1>
-
-    <router-outlet />
-  `,
+  templateUrl: './app.component.html',
   styles: [],
 })
 export class AppComponent implements OnInit {
   pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon | undefined;
 
   ngOnInit() {
     console.table(this.pokemonList);
-    this.selectPokemon(this.pokemonList[0]);
   }
 
-  selectPokemon(pokemon: Pokemon) {
-    console.log(`Vous avez choisi ${pokemon.name}`);
+  selectPokemon(pokemonId: string) {
+    const pokemon: Pokemon | undefined = this.pokemonList.find(p => p.id === +pokemonId);
+
+    if (pokemon) {
+      console.log(`Vous avez choisi ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    }
+    else {
+      console.error(`Aucun pokémon trouvé avec l'identifiant ${pokemonId}`);
+      console.error("Vous avez demandé un pokémon qui n'existe pas.");
+      this.pokemonSelected = pokemon;
+    }
   }
 }
