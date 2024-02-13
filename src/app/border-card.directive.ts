8b585d08a3d58cc7454264e89f7a5ed54928b817
Carole Gérard <gerard.carole78@gmail.com>
2024-02-13 13:22:53 +0100

    Les Directives : Ajouter un paramètre à la directive d'attribut

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[pokemonBorderCard]',
@@ -11,8 +11,10 @@ export class BorderCardDirective {
     this.setBorder('#f5f5f5');
   }
 
+  @Input('pokemonBorderCard') borderColor: string;
+
   @HostListener('mouseenter') onMouseEnter() {
-    this.setBorder('#009688');
+    this.setBorder(this.borderColor || '#009688');
   }
 
   @HostListener('mouseleave') onMouseLeave() {
