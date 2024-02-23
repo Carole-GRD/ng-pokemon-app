0de63ba7d8b459eb127671109fa81f73648c5ce8
Carole Gérard <gerard.carole78@gmail.com>
2024-02-23 14:22:40 +0100

    clean code auth.guard.ts

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -3,40 +3,15 @@ import { AuthService } from './auth.service';
 import { inject } from '@angular/core';
 
 export const AuthGuard = () => {
-  console.log('AuthGuard');
+  // console.log('AuthGuard');
 
   const user = inject(AuthService);
   const router = inject(Router);
 
-  if(!user.isLoggedIn) {
-    router.navigateByUrl('/login')
-    return false
+  if (user.isLoggedIn) {
+    return true;
   }
-  return true
-
+  router.navigateByUrl('/login')
+  return false;
 };
 
-
-// import { Injectable } from '@angular/core';
-// import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot }
-//     from '@angular/router';
-// import { AuthService } from './auth.service';
-  
-// @Injectable()
-// export class AuthGuard implements CanActivate {
-  
-//     constructor(private authService: AuthService, private router: Router) { }
-  
-//     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-//         let url: string = state.url;
-//         return this.checkLogin(url);
-//     }
-  
-//     checkLogin(url: string): boolean {
-//         if (this.authService.isLoggedIn) { return true; }
-//         this.authService.redirectUrl = url;
-//         this.router.navigate(['/login']);
-  
-//         return false;
-//     }
-// }
